4d47c9ccd801c84c772e846b5c0e19b66637a90b
David Björklund <david.bjorklund@gmail.com>
2012-05-01 18:43:24 +0300

    Thin one

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-var data = "module.exports = function () { console.log(\"var data = \\\"%s\\\"\\n%s\", data.replace(/\\\\/g, \"\\\\\\\\\").replace(/\"/g, \"\\\\\\\"\"), data.replace(/{/, \"{\\n   \").replace(/}$/, \"\\n}\")); }"
-module.exports = function () {
-    console.log("var data = \"%s\"\n%s", data.replace(/\\/g, "\\\\").replace(/"/g, "\\\""), data.replace(/{/, "{\n   ").replace(/}$/, "\n}"));
-}
+function quine() { console.log(quine+"") }
